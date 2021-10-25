5c980a721ea8fce8d8b246f4f311da33c4f6a755
Anna <an9062@gmail.com>
2021-10-25 21:04:13 +1100

    added tests for Employee, Manager, Engineer and Intern

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,17 +1,67 @@
+const { describe, it, expect } = require("@jest/globals");
 const Employee = require("./lib/Employee");
 
 // Initialisation
+describe("Initialisation", () => {
+  it("Return an object", () => {
+    const emp = new Employee();
+    expect(typeof emp === object).toEqual(true);
+  });
 
-// Name
+  //Name
+  it("Set a name", () => {
+    const name = "Anna";
+    const emp = new Employee(name);
+    expect(emp.name).toEqual(name);
+  });
 
-// ID
+  //ID
+  it("Set an ID", () => {
+    const id = "1234";
+    const emp = new Employee(id);
+    expect(emp.id).toEqual(id);
+  });
 
-// Email
+  //Email
+  it("Set an email", () => {
+    const email = "a@test.com";
+    const emp = new Employee(email);
+    expect(emp.email).toEqual(email);
+  });
 
-// getName()
+  // getName()
+  describe("getName", () => {
+    it("Retrieve name via getName()", () => {
+      const nameVal = "Anna";
+      const emp = new Employee(nameVal);
+      expect(emp.getName()).toEqual(nameVal);
+    });
+  });
 
-// getId()
+  // getId()
+  describe("getId", () => {
+    it("Retrieve id via getId()", () => {
+      const idVal = "1234";
+      const emp = new Employee(idVal);
+      expect(emp.getId()).toEqual(idVal);
+    });
+  });
 
-// getEmail()
+  // getEmail()
+  describe("getEmail", () => {
+    it("Retrieve email via getEmail()", () => {
+      const emailVal = "a@test.com";
+      const emp = new Employee(emailVal);
+      expect(emp.getEmail()).toEqual(emailVal);
+    });
+  });
 
-// getRole()
+  // getRole()
+  describe("getRole", () => {
+    it("Retrieve role via getRole()", () => {
+      const roleVal = "Employee";
+      const emp = new Employee(roleVal);
+      expect(emp.getRole()).toEqual(roleVal);
+    });
+  });
+});
---
+++ tests/Manager.test.js
@@ -0,0 +1,26 @@
+const Manager = require(".lib/Manager");
+const { it, describe, expect } = require("@jest/globals");
+
+describe("Manager", () => {
+  it("sets officeNumber via constructor", () => {
+    const officeNumber = 12346789;
+    const emp = new Manager("Anna", "1234", "a@test.com", officeNumber);
+    expect(emp.officeNumber).toEqual(officeNumber);
+  });
+
+  describe("getOfficeNumber", () => {
+    it("retrieves office number from getOfficeNumber()", () => {
+      const officeNumberVal = 12345678;
+      const emp = new Manager("Anna", "1234", "a@test.com", officeNumberVal);
+      expect(emp.getOfficeNumber()).toEqual(officeNumber);
+    });
+  });
+
+  describe("getRole", () => {
+    it("retrieves role from getRole()", () => {
+      const roleVal = "Manager";
+      const emp = new Manager("Anna", "1234", "a@test.com", 12345678);
+      expect(emp.getRole()).toEqual(roleVal);
+    });
+  });
+});
